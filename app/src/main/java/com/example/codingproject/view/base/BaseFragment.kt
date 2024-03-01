e0297ac0c5e5875fe4c6a32beeb1c39ea615b39d
palakmittal <palakmittal219@gmail.com>
2024-03-01 21:55:01 +0530

    remove unused imports

--- app/src/main/java/com/example/codingproject/view/base/BaseFragment.kt
+++ app/src/main/java/com/example/codingproject/view/base/BaseFragment.kt
@@ -5,13 +5,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.widget.AppCompatTextView
-import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import androidx.viewbinding.ViewBinding
-import com.example.codingproject.R
 
 abstract class BaseFragment<VB : ViewBinding> : Fragment() {
 
